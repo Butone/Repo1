77e5835ab6fbb315efc7de9ba6c4c93545d2f82b
Butone <mutombo.mutombo@msa.hinet.net>
2019-10-08 15:33:45 +0800

    Init TempC.c

---
+++ TempC.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : TempC.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!\n"); /* prints !!!Hello World!!! */
+	printf("Hi Elephant!\n");
+	getchar();
+	return EXIT_SUCCESS;
+}
